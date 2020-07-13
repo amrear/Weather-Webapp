3372f783d52b7c1a7ecc6abc8e302e57eba9a705
Amirreza A <arjmandamirreza@gmail.com>
2020-07-13 23:50:21 +0430

    added some commenting and fixed some bugs

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 
 * Flask                   1.1.2 
 * requests                2.22.0 
-* **An API key from openweathermap.org**
+* **An API key from** openweathermap.org
 
-**Make sure you set the API key you get from openweathermap.org to `openweather_appid` in your environmet variables.**
+**Make sure you set the API key you get from** openweathermap.org **to `openweather_appid` in your environmet variables.**
--- application.py
+++ application.py
@@ -1,8 +1,11 @@
 import requests  # For web APIs.
 import random
 import os  # For random wallpaper each time.
+import sys
 from flask import Flask, render_template, request
 
+# A list of backgrounds that you want in your web page.
+# You can add more or remove some in `static/background` or changing the specified path.
 background_names = os.listdir(os.path.join(os.getcwd(), "static/backgrounds"))
 
 try:
@@ -10,15 +13,25 @@ try:
     APPID = os.environ["openweather_appid"]
 except:
     Exception("Make sure you've set the required environment variables")
+    sys.exit(1)
 
 def get_location(ip):
+    """
+    Finds geographical location based on the IP address that was passed to it by the help of a wreb api.
+    This function is only called when the location is not provided by the user.
+    """
     ip_location = requests.get(f"http://ip-api.com/json/{ip}").json()
     if ip_location["status"] == "success":
         return ip_location["city"]
 
 
 def get_weather(city, units):
-    weather_api = requests.get("http://api.openweathermap.org/data/2.5/weather",
+    """
+    Fetches the data about the weather based on location if it finds any, then returns it in the form of a dictionary.
+    :param city: The city of your choise which you want the weather of
+    :param units: can be ether `metric` or `imperial` units
+    """
+    weather_api = requests.get("http://api.openweathermap.org/data/2.5/weather",          # Requests the weather API
                                params={"q": city, "units": units, "appid": APPID}).json()
 
     if weather_api.get("message") == "city not found":
@@ -46,13 +59,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    city = request.args.get("city")
-    units = request.args.get("units") if request.args.get(
-        "units") else "metric"
-    if not city:
-        city = get_location("151.233.3.129")  # request.remote_addr
-        if not city:
-            return render_template("city_not_found.html", background=random.choice(background_names))
+    city = request.args.get("city", get_location(request.remote_addr))   # Gets the city that user provided. If nothing is provided,
+                                                                         # the location will be based on clients IP address.
+    
+    units = request.args.get("units")        # Checks to see what unit is specified by the user
+                                             # If nothing is provided, default is `metric`
 
     data = get_weather(city, units)
 
